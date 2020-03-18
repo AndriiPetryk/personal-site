a6d1313713b4feed6960c9e7ab526d1984118944
Andrii Petryk <andrii.petryk.dev@gmail.com>
2020-03-18 19:51:11 +0200

    fixed issue with mongo connection fixed port

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,3 @@
 export const MONGODB_URI = 'mongodb://localhost/portfolioDB';
 export const MONGODB_URI_ATLAS =
-  'mongodb+srv://user:123qweasdzxc@cluster0-fbpnp.mongodb.net/portfolioDB?retryWrites=true&w=majority';
+  'mongodb://user:123qweasdzxc@cluster0-fbpnp.mongodb.net/portfolioDB?retryWrites=true&w=majority';
