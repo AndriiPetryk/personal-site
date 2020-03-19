cd751decec6061cba9da8ba4c93a00941006339c
Andrii Petryk <andrii.petryk.dev@gmail.com>
2020-03-19 20:54:14 +0200

    fixed database

--- util/dbStore.js
+++ util/dbStore.js
@@ -1,10 +1,10 @@
 import session from 'express-session';
-import { MONGODB_URI } from '../constants/constants';
+import { MONGODB_URI_ATLAS } from '../constants/constants';
 
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
+  uri: MONGODB_URI_ATLAS,
   collection: 'sessions'
 });
 
